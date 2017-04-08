05369406420e78d12fc9d567c18b952bbb9d21a0
Unknown <jmpcba@gmail.com>
2017-04-08 13:09:02 -0300

    probando nodes

--- run.py
+++ run.py
@@ -1,4 +1,16 @@
+import os
 #python script that will be run from jenkins
 
+lines = ['/usr/bin/python\n', '\n', 'print "este es un artefacto"']
+
+try:
+    os.remove('artefacto.txt')
+except FileNotFoundError as e:
+    print e.message
+
 print "hello GIT"
-print "crease o no funciona"
+
+with open('artefacto.txt', 'w') as f
+    for line in lines:
+        f.write(line)
+    
