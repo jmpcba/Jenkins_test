9bf6e32dddf47d1e08be912bd57360ce73c312d6
Unknown <jmpcba@gmail.com>
2017-04-08 14:23:53 -0300

    missing shebang

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import os
 #python script that will be run from jenkins
 
-lines = ['/usr/bin/python\n', '\n', 'print "este es un artefacto"']
+lines = ['#!/usr/bin/python\n', '\n', 'print "este es un artefacto"']
 file_path = os.path.join(os.environ['WORKSPACE'],'artefacto.py')
 try:
     os.remove(file_path)
