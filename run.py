1195dfdde6359a4461128b914dc79dde729e7677
Unknown <jmpcba@gmail.com>
2017-04-08 14:06:20 -0300

    artefacto.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 #python script that will be run from jenkins
 
 lines = ['/usr/bin/python\n', '\n', 'print "este es un artefacto"']
-file_path = os.path.join(os.environ['WORKSPACE'],'artefacto.txt')
+file_path = os.path.join(os.environ['WORKSPACE'],'artefacto.py')
 try:
     os.remove(file_path)
 except OSError as e:
